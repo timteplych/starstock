9532e752f3c8145f36a9185534639cd53d7d6a10
timteplych <timteplych@mail.ru>
2018-08-04 18:26:51 +0800

    project customization

---
+++ src/main/java/ru/ttv/config/WebAppInitializer.java
@@ -0,0 +1,18 @@
+package ru.ttv.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class,WebConfig.class);
+        context.setServletContext(servletContext);
+
+        servletContext.addServlet()
+    }
+}
--- src/main/java/ru/ttv/config/WebConfig.java
+++ src/main/java/ru/ttv/config/WebConfig.java
@@ -1,8 +1,29 @@
 package ru.ttv.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver getViewResolver(){
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setOrder(1);
+        freeMarkerViewResolver.setSuffix(".ftl");
+        freeMarkerViewResolver.setPrefix("");
+        return freeMarkerViewResolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer getFreeMarkerConfigurer(){
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPaths("/","WEB-INF/views");
+        return freeMarkerConfigurer;
+    }
 }
