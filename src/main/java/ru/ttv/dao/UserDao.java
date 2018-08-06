614b8bf946b05b28d4789ef18591cc6a7291e4d0
timteplych <timteplych@mail.ru>
2018-08-06 22:42:09 +0800

    added crud operations

--- src/main/java/ru/ttv/dao/UserDao.java
+++ src/main/java/ru/ttv/dao/UserDao.java
@@ -5,5 +5,10 @@ import ru.ttv.entity.User;
 import java.util.List;
 
 public interface UserDao {
+
+    void save(User user);
+    User getById(int id);
+    void update (User user);
     List<User> findAll();
+    void delete(int id);
 }
--- src/main/java/ru/ttv/dao/UserDaoImpl.java
+++ src/main/java/ru/ttv/dao/UserDaoImpl.java
@@ -7,7 +7,7 @@ import ru.ttv.mapper.UserMapper;
 
 import java.util.List;
 
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl implements UserDao{
     public final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -16,8 +16,29 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
+    public void save(User user) {
+        String sql = "INSERT INTO user (username,email,age) VALUES (?,?,?)";
+        jdbcTemplate.update(sql,user.getName(),user.getEmail(),user.getAge());
+    }
+
+    @Override
+    public User getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
     public List<User> findAll() {
         String sql = "SELECT * FROM users";
         return jdbcTemplate.query(sql, new UserMapper());
+    }
+
+    @Override
+    public void delete(int id) {
+
     }
 }
